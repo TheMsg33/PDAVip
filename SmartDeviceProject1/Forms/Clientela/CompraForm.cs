832805ce2c4307fdc41cabc0f654b9b1437e9c5f
TheMsg33 <jogos.wix.com2011@gmail.com>
2018-08-10 17:20:20 +0100

    .

--- SmartDeviceProject1/Forms/Clientela/CompraForm.cs
+++ SmartDeviceProject1/Forms/Clientela/CompraForm.cs
@@ -6,14 +6,25 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Collections;
+using SmartDeviceProject1.Class;
 
 namespace SmartDeviceProject1.Forms.Clientela
 {
     public partial class CompraForm : Form
     {
+
+        public List<ProdutoCompra> listProd;
+        
         public CompraForm()
         {
             InitializeComponent();
+        }
+
+
+        private void CompraForm_Load(object sender, EventArgs e)
+        {
+            listProd = new List<ProdutoCompra>();
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
@@ -27,6 +38,16 @@ namespace SmartDeviceProject1.Forms.Clientela
         }
 
         private void button10_GotFocus(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
 
         }
